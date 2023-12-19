24663c51e1a3ccc96b2436efa715d4f606ea9255
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2023-12-19 11:19:51 -0500

    Structuring env variables in kafka fc

--- src/components/kafka/kafka/kafka-client.tsx
+++ src/components/kafka/kafka/kafka-client.tsx
@@ -1,10 +1,21 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { useMenuGlobalContext } from "../../header/context/globalmenucontext";
 import { Kafka, KafkaConfig } from "kafkajs";
+
+
+//create a interface data type for the env variables
+interface envData {
+    kafka_client_cert: string;
+    kafka_client_cert_key: string;
+    kafka_trusted_cert: string;
+    kafka_url: string;
+
+}
 
 const KafkaClient: FC = () => {
 
     const { menu } = useMenuGlobalContext();
+    const [env, setEnv] = useState<envData | undefined>(undefined);
     // create a kafka config with TLS enabled
     // const kafkaConfig: KafkaConfig = ({
     //     clientId: 'my-app',
@@ -28,6 +39,14 @@ const KafkaClient: FC = () => {
             .then(response => response.json())
             .then(data => {
                 console.log("KafkaClientenv variables", data)
+                // parse the data and set it to envData interface variable
+                const envData: envData = data
+                // set the envData to the state variable env
+                setEnv(envData)
+                //console log the envData
+                console.log("KafkaClientenvData", envData.kafka_url)
+                console.log("KafkaClientenvData", envData.kafka_client_cert)
+
             })
             .catch(error => {
                 console.error(error)
@@ -52,3 +71,5 @@ const KafkaClient: FC = () => {
 }
 
 export default KafkaClient;
+
+    
