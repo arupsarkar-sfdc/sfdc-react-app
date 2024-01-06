53b787b181d8babed97a57e8c82023ed985fbfb2
arupsarkar <sarkar.arup@gmail.com>
2024-01-06 08:13:46 +0530

    useEffect code reinstated

--- src/components/header/auth/authenticate.tsx
+++ src/components/header/auth/authenticate.tsx
@@ -25,43 +25,43 @@ const Authenticate: FC = () => {
     try {
       console.log("authenticate - useEffect - start");
       console.log("authenticate - useEffect - end");
-    //   const source = new EventSource(`/auth/token`);
-    //   source.addEventListener(
-    //     "open",
-    //     () => {
-    //       console.log("SSE opened!");
-    //     },
-    //     false
-    //   );
+      const source = new EventSource(`/auth/token`);
+      source.addEventListener(
+        "open",
+        () => {
+          console.log("SSE opened!");
+        },
+        false
+      );
 
-    //   source.addEventListener(
-    //     "message",
-    //     (e: any) => {
-    //       console.log("SSE payload received ", e.data);
-    //       setProgress(false)
-    //       if (e.data == "LoggedIn") {
-    //         console.log("Hide the login button", e.data == "LoggedIn");
-    //         setAuth(true);
-    //       } else if (e.data == "NotLoggedIn") {
-    //         console.log(
-    //           "Do not hide the login button",
-    //           e.data == "NotLoggedIn"
-    //         );
-    //         setAuth(false);
-    //       }
-    //     },
-    //     false
-    //   );
-    //   source.addEventListener(
-    //     "error",
-    //     (e) => {
-    //       console.error("Error: ", e);
-    //     },
-    //     false
-    //   );
-    //   return () => {
-    //     source.close();
-    //   };
+      source.addEventListener(
+        "message",
+        (e: any) => {
+          console.log("SSE payload received ", e.data);
+          setProgress(false)
+          if (e.data == "LoggedIn") {
+            console.log("Hide the login button", e.data == "LoggedIn");
+            setAuth(true);
+          } else if (e.data == "NotLoggedIn") {
+            console.log(
+              "Do not hide the login button",
+              e.data == "NotLoggedIn"
+            );
+            setAuth(false);
+          }
+        },
+        false
+      );
+      source.addEventListener(
+        "error",
+        (e) => {
+          console.error("Error: ", e);
+        },
+        false
+      );
+      return () => {
+        source.close();
+      };
     } catch (error) {
       console.error("Error fetching server side events in login event ", error);
     }
